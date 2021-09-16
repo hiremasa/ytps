699fcefadf2591ca25c21c6f0349051e79a33dcd
hiremasa <syorikunn0901@gmail.com>
2021-09-16 19:55:47 +0900

    update Simple_Preprocessing_GLS.py

--- homeworks/Lightcurve_preparation/Simple_Preprocessing_GLS.py
+++ homeworks/Lightcurve_preparation/Simple_Preprocessing_GLS.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 import matplotlib
@@ -13,6 +14,7 @@ from astropy.timeseries import LombScargle
 import astropy.units as u
 from gls import Gls
 import argparse
+from glob import glob
 
 
 parser = argparse.ArgumentParser(description='Output Fig & CSV file from one star by\
@@ -29,8 +31,17 @@ parser.add_argument('--Pbeg', type=float, default=0.1,
                     help='minimumn P(period) value (default: 0.1)')
 parser.add_argument('--Pend', type=float, default=10,
                     help='maximumn P(period) value (default: 10)')
+parser.add_argument('--verbose', action="store_true", default=False,
+                    help='verbose (default: False)')
+parser.add_argument('--collect', type=bool, default=False,
+                    help='collect all outputs (default: False)')
 
 args = parser.parse_args()
+
+if not os.path.exists("output"):
+    os.makedirs("output")
+    os.makedirs("output/images")
+    os.makedirs("output/dataframes")
 
 def plot_4figures(lc, lc_clean, gls, preds):
     fig, axes = plt.subplots(2, 2, figsize=(10,10), tight_layout=True, facecolor="whitesmoke")
@@ -58,7 +69,6 @@ def plot_4figures(lc, lc_clean, gls, preds):
         else:
             ax.axvline(x=preds["P"]*m , linestyle=":", color="black")
             ax.axvline(x=preds["P"]/m , linestyle=":", color="black")
-    
     ax.legend()
 
     ax.set_title("Peridogram", fontsize=20)
@@ -79,17 +89,44 @@ def plot_4figures(lc, lc_clean, gls, preds):
 
 
 if __name__ == "__main__":
-    target_star = args.target_star
-    lc_file = lk.search_lightcurve(args.target_star, author=args.author, exptime=args.exptime)
-    lc = lc_file[0].download()
-    lc_clean = lc.normalize().remove_nans().remove_outliers(sigma=args.exptime)
 
-    df = [lc_clean.time.value, lc_clean.flux.value, lc_clean.flux_err.value]
-    gls = Gls(df, Pbeg=args.Pbeg, Pend=args.Pend)
-    preds = gls.best
+    if args.collect:
+        files = glob("output/dataframes/TOI*.csv")
+        files.sort()
 
-    #save the results
-    fig = plot_4figures(lc, lc_clean, gls, preds)
-    fig.savefig(f"{args.target_star}_{lc.SECTOR}.png")
-    pd.Series(preds).to_csv(f"{args.target_star}.csv")
+        ss = []
+        for f in files:
+            s = pd.read_csv(f, squeeze=True, index_col=0)
+            ss.append(s)
+
+        all_df = pd.concat(ss, axis=1)
+        all_df.to_csv("all_df.csv")
+
+    else:
+        target_star = args.target_star
+
+        if args.verbose:
+            print(f"***Searching for {args.target_star}")
+        try:
+            lc_file = lk.search_lightcurve(args.target_star, author=args.author, exptime=args.exptime)
+            if len(lc_file) < 1:
+                raise ValueError("Warning: No Light Curves found")
+            lc = lc_file[0].download()
+            lc_clean = lc.normalize().remove_nans().remove_outliers(sigma=args.exptime)
+
+            df = [lc_clean.time.value, lc_clean.flux.value, lc_clean.flux_err.value]
+            if args.verbose:
+                print("Running GLS")
+            gls = Gls(df, Pbeg=args.Pbeg, Pend=args.Pend)
+            preds = gls.best
+            preds["pmax"] = gls.pmax
+
+            #save the results
+            fig = plot_4figures(lc, lc_clean, gls, preds)
+            fig.savefig(f"./output/images/{args.target_star}_SECTOR{lc.SECTOR}.png")
+            pd.Series(preds, name=args.target_star).to_csv(f"./output/dataframes/{args.target_star}.csv")
+            print("Successfully Finished!!")
+        except Exception as e:
+            print("Warning: ", e)
+        print("==================================================")
 
