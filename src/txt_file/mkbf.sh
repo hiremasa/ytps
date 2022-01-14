d612d474dd8199d8619f8a289a632b1fe930423e
hiremasa <syorikunn0901@gmail.com>
2022-01-14 17:48:02 +0900

    add scripts for sector 45

--- src/txt_file/mkbf.sh
+++ src/txt_file/mkbf.sh
@@ -1,1 +1,1 @@
-cat all_targets_S041_v1.txt | awk -F " " '{print $1}' | while read tic; do echo python simple_preprocessing_gls.py --TIC $tic --verbose --sector_number 41 --experiment_name sector41; done > sector41.batch
+cat txt_file/all_targets_S045_v1.txt | awk -F " " '{print $1}' | while read tic; do echo python simple_preprocessing_gls.py --TIC $tic --verbose --sector_number 45 --experiment_name sector45; done > txt_file/sector45.batch
